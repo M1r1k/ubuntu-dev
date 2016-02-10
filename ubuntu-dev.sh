632bdeec66490449622aea42cb693fce16ce0566
Artem Miroshnyk <miroshnik1992@gmail.com>
2016-02-10 21:42:45 +0100

    Fix all flow errors

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -1,9 +1,11 @@
 # Install Ansible
 sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible
 
 # Run ansible-playbook
-ansible-galaxy install -r requirements.txt
-ansible-playbook -K ubuntu-dev.yml
+sudo ansible-galaxy install -r requirements.txt
+sudo ansible-playbook -K ubuntu-dev.yml
+
+sudo find ~ -name ".*" -exec chown $USER:$USER {} \;
